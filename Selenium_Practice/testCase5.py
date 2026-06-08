5b58920a89d2444eab1d0fb42ae29cd02a95dab1
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-08 13:11:37 +0530

    new programs added

--- Selenium_Practice/testCase5.py
+++ Selenium_Practice/testCase5.py
@@ -2,31 +2,38 @@ from selenium import webdriver as wb
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.chrome.options import Options
 
-driver = wb.Chrome()
-driver.maximize_window()
+chrome_options = Options()
+chrome_options.add_argument("--window-size=1920,1080")  
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--headless=new")           
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = wb.Chrome(options=chrome_options)
 driver.get("http://automationexercise.com")
-wait = WebDriverWait(driver,30)
+wait = WebDriverWait(driver, 30)
 
-signUpBtn = wait.until(expected_conditions.visibility_of_element_located((By.XPATH,"//div[@class='col-sm-8']/div/ul/child::li[4]")))
-assert signUpBtn.text == "Signup / Login", "Home page not successfully opened"
+signUpBtn = wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//a[contains(text(), 'Signup / Login')]")))
+assert "Signup / Login" in signUpBtn.text, "Home page not successfully opened"
 signUpBtn.click()
 
-text = wait.until(expected_conditions.visibility_of_element_located((By.XPATH,"//div[@class='signup-form']/child::h2")))
-assert text.text == "New User Signup!","Sign up page not loaded"
+text = wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@class='signup-form']/h2")))
+assert text.text == "New User Signup!", "Sign up page not loaded"
 
-name = driver.find_element(By.XPATH,value="//input[@name='name']")
+name = driver.find_element(By.XPATH, value="//input[@data-qa='signup-name']")
 name.send_keys("messi")
 
-email = driver.find_element(By.XPATH,value='//*[@id="form"]/div/div/div[3]/div/form/input[3]')
+email = driver.find_element(By.XPATH, value="//input[@data-qa='signup-email']")
 email.send_keys("messi10@gmail.com")
 
-signup = driver.find_element(By.XPATH,value="//button[contains(text(),'Signup')]")
+signup = driver.find_element(By.XPATH, value="//button[@data-qa='signup-button']")
 signup.click()
 
-errortext = wait.until(expected_conditions.visibility_of_element_located((By.XPATH,"//*[@id='form']/div/div/div[3]/div/form/p")))
-assert errortext.text == "Email Address already exist!","Message is not displayed"
+errortext = wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//p[text()='Email Address already exist!']")))
+assert errortext.text == "Email Address already exist!", "Message is not displayed"
+
 print("Test Case Passed")
-
-
-
+driver.quit()
