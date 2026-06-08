aeab518d51dc02f34d19b5ffe99c124eb85d6eb0
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-08 12:57:00 +0530

    new files added

--- Selenium_Practice/testCase2.py
+++ Selenium_Practice/testCase2.py
@@ -14,13 +14,14 @@ try:
     driver.find_element(By.XPATH, "//a[@href = \"/login\"]").click()
     assert wait.until(EC.visibility_of_element_located((By.XPATH, "//h2[text() = \"Login to your account\"]"))).is_displayed(), "Login page is not reached"
     print("Login text is verified")
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@data-qa = \"login-email\"]"))).send_keys("messi710@gmail.com")
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@data-qa = \"login-email\"]"))).send_keys("messi1@gmail.com")
     driver.find_element(By.XPATH, "//input[@data-qa = \"login-password\"]").send_keys("messi10")
     driver.find_element(By.XPATH, "//button[@data-qa = \"login-button\"]").click()
 
     assert wait.until(EC.visibility_of_element_located((By.XPATH, "//i/following-sibling::b"))).text == "messi", "Cannot login"
     print("Login Successfull")
-    driver.find_element(By.XPATH, "//a[@href = \"/delete_account\"]").click()
+    delBtn = driver.find_element(By.XPATH, "//a[@href = \"/delete_account\"]")
+    driver.execute_script("arguments[0].click();",delBtn)
 
     assert wait.until(EC.visibility_of_element_located((By.XPATH, "//b[text() = \"Account Deleted!\"]"))).is_displayed(), "Cannot delete account"
     print("Account deleted successfully.")
