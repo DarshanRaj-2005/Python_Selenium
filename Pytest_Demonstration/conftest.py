b8423f2df3f3672ad580da1f97a1254a641c4920
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-08 20:38:37 +0530

    new files added

--- Pytest_Demonstration/conftest.py
+++ Pytest_Demonstration/conftest.py
@@ -1,16 +1,18 @@
-from urllib import request
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium import webdriver
 import pytest
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture()
-def setUp(request):
-    driver = webdriver.Chrome()
+def setUp():
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(5)
     driver.get("https://tutorialsninja.com/demo/")
-    wait = WebDriverWait(driver,30)
-    request.cls.driver = driver
-    yield driver,wait
+    
+    wait = WebDriverWait(driver, 30)
+    yield driver, wait 
     driver.quit()
