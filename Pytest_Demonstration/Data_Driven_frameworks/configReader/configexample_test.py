b78b47d295e0d2a82ba1d750f6b4d66d8b46c67a
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-09 14:35:42 +0530

    new files added

--- Pytest_Demonstration/Data_Driven_frameworks/configReader/configexample_test.py
+++ Pytest_Demonstration/Data_Driven_frameworks/configReader/configexample_test.py
@@ -1,4 +1,4 @@
-import read_config
+import configreader
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium import webdriver
@@ -7,20 +7,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 def test_validLogin():
         driver = webdriver.Chrome()
-        driver.get(read_config.getData("info","url"))
+        driver.get(configreader.getDatas("info","url"))
         driver.maximize_window()
         wait = WebDriverWait(driver,30)
-        account = driver.find_element(By.XPATH,"//*[@id='top-links']/ul/li[2]/a")
+        account = driver.find_element(By.XPATH,"//div[@id='top-links']/ul/li[2]/a")
         account.click()
 
         login = wait.until(expected_conditions.visibility_of_element_located((By.XPATH,"//*[@id='top-links']/ul/li[2]/ul/li[2]/a")))
         login.click()
 
         username = driver.find_element(By.XPATH,"//*[@id='input-email']")
-        username.send_keys(read_config.getData("credentials","username"))
+        username.send_keys(configreader.getDatas("credentials","username"))
 
         password = driver.find_element(By.XPATH,"//*[@id='input-password']")
-        password.send_keys(read_config.getData("credentials","password"))
+        password.send_keys(configreader.getDatas("credentials","password"))
 
         submit = driver.find_element(By.XPATH,"//*[@id='content']/div/div[2]/div/form/input")
         submit.click()
--- Pytest_Demonstration/Data_Driven_frameworks/configReader/configreader.py
+++ Pytest_Demonstration/Data_Driven_frameworks/configReader/configreader.py
@@ -2,7 +2,7 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 config.read("./Data_Driven_frameworks/configReader/config.ini")
-def getData(category, key):
+def getDatas(category, key):
     value = config.get(category, key)
     return value
 
